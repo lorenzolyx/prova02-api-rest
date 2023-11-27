02531576e616059219da7ef3c4283a88a36c3f20
Lorenzo <lorenzo1324enzo@gmail.com>
2023-11-27 20:59:46 -0300

    Commit Final

--- src/routes/reservas_routes.py
+++ src/routes/reservas_routes.py
@@ -1,6 +1,7 @@
 import random
 
-from fastapi import APIRouter
+from fastapi import HTTPException
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from sqlmodel import select
 
@@ -32,9 +33,19 @@ def cria_reserva(reserva: Reserva):
 
         # TODO - Validar se existe uma reserva para o mesmo documento
 
+        reserva_existente = (
+            session.exec(
+                select(Reserva).where(Reserva.documento == reserva.documento)
+            ).first()
+            )
         codigo_reserva = "".join(
             [str(random.randint(0, 999)).zfill(3) for _ in range(2)]
         )
+        if reserva_existente:
+            return JSONResponse(
+                content={"message": f"Já existe uma reserva com este documento {reserva.voo_id}."},
+                status_code=400,
+            )
 
         reserva.codigo_reserva = codigo_reserva
         session.add(reserva)
@@ -44,8 +55,42 @@ def cria_reserva(reserva: Reserva):
 
 
 @reservas_router.post("/{codigo_reserva}/checkin/{num_poltrona}")
-def faz_checkin(codigo_reserva: str, num_poltrona: int):
-    # TODO - Implementar reserva de poltrona
+def checkin(codigo_reserva: str, num_poltrona: int):
     pass
 
-# TODO - Implementar troca de reserva de poltrona
+    # TODO - Implementar reserva de poltrona
+@reservas_router.post("/{codigo_reserva}/checkin/{num_poltrona}")
+def checkin(codigo_reserva: str, num_poltrona: int):
+    with get_session() as session:
+        reserva = session.exec(select(Reserva).where(Reserva.codigo_reserva == codigo_reserva)).first()
+    
+    if not reserva:
+            return JSONResponse(
+                content={"message": f"Reserva com código {codigo_reserva} não encontrada."},
+                status_code=404,
+            )
+
+    if reserva.num_poltrona is not None:
+            return JSONResponse(
+                content={"message": f"Poltrona {num_poltrona} ocupada."},
+                status_code=400,
+            )
+    
+    # TODO - Implementar troca de reserva de poltrona
+
+@reservas_router.patch("/{codigo_reserva}/checkin/{num_poltrona}")
+def checkin(codigo_reserva: str, num_poltrona: int):
+    with get_session() as session:
+        reserva = session.exec(select(Reserva).where(Reserva.codigo_reserva == codigo_reserva)).first()
+
+    if not reserva:
+        raise HTTPException(status_code=404, detail=f"A reserva com o código {codigo_reserva} não foi encontrada.")
+
+    if reserva.num_poltrona is not None:
+        raise HTTPException(status_code=400, detail=f"Poltrona {num_poltrona} encontra-se ocupada.")
+
+    reserva.num_poltrona = num_poltrona
+    session.commit()
+    session.refresh(reserva)
+
+    return reserva
